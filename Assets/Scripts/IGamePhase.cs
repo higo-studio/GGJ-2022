5b4946786fcfabb64cfd46f94835055c0f48456d
mooooooi <emwings@outlook.com>
2022-01-22 11:42:11 +0800

    feat: add IGamePhase

---
+++ Assets/Scripts/IGamePhase.cs
@@ -0,0 +1,20 @@
+public struct PlayerInput
+{
+    public int horizontal;
+    public int vertical;
+    public bool applyRotate;
+    public bool IsValid => horizontal != 0 || vertical != 0 || applyRotate;
+}
+public enum Role
+{
+    Black,
+    White
+}
+
+public interface IGamePhase
+{
+    void Init(float step);
+    // 0
+    // ÖðÖ¡µ÷ÓÃ
+    void Update(float time, PlayerInput[] input, out int[,] cells);
+}
