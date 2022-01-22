20ec3b15c5f59d8001352a425daa45be304f6451
mooooooi <emwings@outlook.com>
2022-01-22 15:54:58 +0800

    feat: add battle

---
+++ Assets/Scripts/TetrisCore/Battle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Battle : MonoBehaviour
+{
+    public Vector2Int MapSize;
+    public Tilemap Tilemap;
+    [Range(0.5f, 2f)]
+    public float Step = 1f;
+    IGamePhase core = new TetrisCore();
+
+    private void Awake()
+    {
+        core.Init(Step, MapSize);
+    }
+}
