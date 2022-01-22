1c1ddfd9f3f2015cce40367131bba537988e1367
mooooooi <emwings@outlook.com>
2022-01-22 16:06:33 +0800

    feat: add core update

--- Assets/Scripts/TetrisCore/Battle.cs
+++ Assets/Scripts/TetrisCore/Battle.cs
@@ -9,8 +9,28 @@ public class Battle : MonoBehaviour
     public float Step = 1f;
     IGamePhase core = new TetrisCore();
 
+    public int[,] RenderCells;
+
+    private PlayerInput[] inputs;
+
+    public RectInt Bounds
+    {
+        get
+        {
+            Vector2Int position = new Vector2Int(-MapSize.x / 2, -MapSize.y / 2);
+            return new RectInt(position, MapSize);
+        }
+    }
+
     private void Awake()
     {
+        RenderCells = new int[MapSize.x, MapSize.y];
+        inputs = new PlayerInput[2];
         core.Init(Step, MapSize);
+    }
+
+    private void FixedUpdate()
+    {
+        core.Update(Time.fixedDeltaTime, inputs, ref RenderCells);
     }
 }
